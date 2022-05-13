a4146ce5406cfc8c446f3950066171837f5ca893
Mike Mendizabal <mendizam@gmatc.matc.edu>
2022-05-12 19:24:06 -0500

    Made progress on Navigation

--- App.js
+++ App.js
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet } from "react-native";
+import Contacts from "./screens/Contacts";
+import Profile from "./screens/Profile";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import TabNavigator from "./components/TabNavigator";
+import DrawerNavigation from "./components/DrawerNavigation";
+import Options from "./screens/Options";
+import * as Linking from "expo-linking";
+
+const Stack = createStackNavigator();
+
+const prefix = Linking.createURL("/");
+const config = {
+  screens: {
+    Tabs: "/",
+    Profile: "contact",
+  },
+};
 
 export default function App() {
+  const linking = {
+    prefixes: [prefix],
+    config,
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <NavigationContainer linking={linking}>
       <StatusBar style="auto" />
-    </View>
+      <Stack.Navigator initialRouteName="Drawer">
+        <Stack.Screen name="Contacts" component={Contacts} />
+        <Stack.Screen
+          name="Profile"
+          component={Profile}
+          options={{ headerBackTitleVisible: false }}
+        />
+        <Stack.Screen
+          name="Options"
+          component={Options}
+          options={{ presentation: "modal" }}
+        />
+        <Stack.Screen
+          name="Drawer"
+          component={DrawerNavigation}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Tabs"
+          component={TabNavigator}
+          options={{ headerShown: false }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
